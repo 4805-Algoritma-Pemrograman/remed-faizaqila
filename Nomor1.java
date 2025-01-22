72bf917510251c4bdd772967dd42d7ed5b5535c8
faizaqila <m.faiz.a2006@gmail.com>
2025-01-22 14:04:24 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,27 @@
 public class Nomor1{
      public static void main(String[] args) {
-        
+
+
+           Scanner scanner = new Scanner(System.in);
+
+       
+        System.out.print("Masukkan total kilometer: ");
+        int totalKm = scanner.nextInt();
+
+       
+        double taxiFare;
+
+       
+        if (totalKm <= 3) {
+            taxiFare = 20000.0; 
+        } else {
+            taxiFare = 20000.0 + (totalKm - 3) * 10000.0; 
+        }
+
+       
+        System.out.println("Total km: " + totalKm);
+        System.out.println("Honor taksi: " + taxiFare);
+
+        scanner.close();
     }
 }
